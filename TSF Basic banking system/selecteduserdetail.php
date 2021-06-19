42c1c56a58167da966a691a99360f6ae697cf48b
Kanchan Chintalwar <56031643+KanchanChintalwar@users.noreply.github.com>
2021-06-19 23:38:46 +0530

    Create selecteduserdetail.php

---
+++ TSF Basic banking system/selecteduserdetail.php
@@ -0,0 +1,239 @@
+<?php
+include 'config.php';
+
+if(isset($_POST['submit']))
+{
+    $from = $_GET['id'];
+    $to = $_POST['to'];
+    $amount = $_POST['amount'];
+
+    $sql = "SELECT * from users where id=$from";
+    $query = mysqli_query($conn,$sql);
+    $sql1 = mysqli_fetch_array($query); // returns array or output of user from which the amount is to be transferred.
+
+    $sql = "SELECT * from users where id=$to";
+    $query = mysqli_query($conn,$sql);
+    $sql2 = mysqli_fetch_array($query);
+
+
+
+    // constraint to check input of negative value by user
+    if (($amount)<0)
+   {
+        echo '<script type="text/javascript">';
+        echo ' alert("Oops! Negative values cannot be transferred")';  // showing an alert box.
+        echo '</script>';
+    }
+
+
+  
+    // constraint to check insufficient balance.
+    else if($amount > $sql1['balance']) 
+    {
+        
+        echo '<script type="text/javascript">';
+        echo ' alert("Bad Luck! Insufficient Balance")';  // showing an alert box.
+        echo '</script>';
+    }
+    
+
+
+    // constraint to check zero values
+    else if($amount == 0){
+
+        echo "<script> alert('Oops ! Zero value cannot be transferred.');
+        
+        </script>";
+     }
+
+
+    else {
+        
+                // deducting amount from sender's account
+                $newbalance = $sql1['balance'] - $amount;
+                $sql = "UPDATE users set balance=$newbalance where id=$from";
+                mysqli_query($conn,$sql);
+             
+
+                // adding amount to reciever's account
+                $newbalance = $sql2['balance'] + $amount;
+                $sql = "UPDATE users set balance=$newbalance where id=$to";
+                mysqli_query($conn,$sql);
+                
+                $sender = $sql1['name'];
+                $receiver = $sql2['name'];
+                $sql = "INSERT INTO transaction(`sender`, `receiver`, `balance`) VALUES ('$sender','$receiver','$amount')";
+                $query=mysqli_query($conn,$sql);
+
+                if($query){
+                     echo "<script> alert('Transaction Successful');
+                                     window.location='transactionhistory.php';
+                           </script>";
+                    
+                }
+
+                $newbalance= 0;
+                $amount =0;
+        }
+    
+}
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Transaction</title>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="css/table.css">
+    <title>Transaction</title>
+
+    <style>
+    	
+		
+        a:hover{
+            background-color:navy;
+            color:white;
+        }
+        *{
+            margin:0;
+            padding:0;
+            box-sizing:border-box;
+        }
+        .transfer{
+            background-color: #5E11A3;
+            border: 2px solid #5E11A3;
+            border-radius:10px;
+            color: white;
+            padding: 5px 50px;
+            text-align: center;
+            display: inline-block;
+            font-size: 20px;
+            margin: 40px 130px;
+            cursor: pointer;
+            text-decoration:none;
+        }
+
+		}
+	    .transfer:hover{
+			background-color:navy;
+            color:white;
+			transform: scale(1.1);
+		}
+        .navbar
+        {
+            width:100%;
+            white-space:nowrap;
+            background-color: #5E11A3;
+        }
+        .main_div
+        {
+            width:100%;
+            height:100vh;
+        
+        }
+
+
+    </style>
+</head>
+
+<body>
+<div class="main_div">
+ 
+ <div class="navbar navbar-expand-md">
+
+  <label href="#" class="navbar-brand font-weight-bold text-white text-center">THE SPARK BANK</label>
+  <button class="navbar-toggler text-white " type="button" data-toggle="collapse" data-target="#collapsenavbar">
+  <span class="navbar-toggler-icon" style="background:white;"></span>
+  </button>
+ 
+   <div class="collapse navbar-collapse text-center" id="collapsenavbar">
+      <ul class="navbar-nav ml-auto">
+          <li class="nav-item">
+              <a href="index.php" class="nav-link text-white">HOME</a></li>
+          
+          <li class="nav-item">
+              <a href="transfermoney.php" class="nav-link text-white">VIEW USERS</a></li>
+          <li class="nav-item">
+              <a href="#" class="nav-link text-white">CONTACT</a></li>    
+           </ul>
+    </div>
+ </div>
+
+
+	<div class="container">
+        <h2 class="text-center pt-4">Transaction</h2>
+            <?php
+                include 'config.php';
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM  users where id=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error : ".$sql."<br>".mysqli_error($conn);
+                }
+                $rows=mysqli_fetch_assoc($result);
+            ?>
+            <form method="post" name="tcredit" class="tabletext" ><br>
+        <div>
+            <table class="table table-striped table-condensed table-bordered">
+                <tr>
+                    <th class="text-center">Id</th>
+                    <th class="text-center">Name</th>
+                    <th class="text-center">Email</th>
+                    <th class="text-center">Account no.</th>
+                    <th class="text-center">Balance</th>
+                </tr>
+                <tr>
+                    <td class="py-2"><?php echo $rows['id'] ?></td>
+                    <td class="py-2"><?php echo $rows['name'] ?></td>
+                    <td class="py-2"><?php echo $rows['email Id'] ?></td>
+                    <td class="py-2"><?php echo $rows['Account no.'] ?></td>
+                    <td class="py-2"><?php echo $rows['balance'] ?></td>
+                </tr>
+            </table>
+        </div>
+        <br>
+        <label>Transfer To:</label>
+        <select name="to" class="form-control" required>
+            <option value="" disabled selected>Choose</option>
+            <?php
+                include 'config.php';
+                $sid=$_GET['id'];
+                $sql = "SELECT * FROM users where id!=$sid";
+                $result=mysqli_query($conn,$sql);
+                if(!$result)
+                {
+                    echo "Error ".$sql."<br>".mysqli_error($conn);
+                }
+                while($rows = mysqli_fetch_assoc($result)) {
+            ?>
+                <option class="table" value="<?php echo $rows['id'];?>" >
+                
+                    <?php echo $rows['name'] ;?> (Balance: 
+                    <?php echo $rows['balance'] ;?> ) 
+               
+                </option>
+            <?php 
+                } 
+            ?>
+            <div>
+        </select>
+        <br>
+        <br>
+            <label>Amount:</label>
+            <input type="number" class="form-control" name="amount" required>   
+            <br><br>
+                <div class="text-center" >
+            <button class="transfer" name="submit" type="submit" id="myBtn">Transfer</button>
+            
+            <a href="transfermoney.php" class="transfer">Back</a>
+            </div>
+        </form>
+    </div>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+</div>
+</body>
+</html>
